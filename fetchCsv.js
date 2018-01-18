92f01afd9de2c0cba6b9f2589107ac48708aeb28
Takashi SASAKI <takashi316@gmail.com>
2018-01-18 20:11:11 +0900

    rewritten using Promise

--- fetchCsv.js
+++ fetchCsv.js
@@ -1,20 +1,28 @@
 var http = require("https");
 var parse = require("csv-parse/lib/sync");
 
-var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFAs12jcTQbXPN3pkZ0-qDV1iAmQ8a438nO8H2mtNri47NI_znqcX_UaxnhjDMUxvi38cq8b9etStk/pub?gid=0&single=true&output=csv";
+var urlBase = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFAs12jcTQbXPN3pkZ0-qDV1iAmQ8a438nO8H2mtNri47NI_znqcX_UaxnhjDMUxvi38cq8b9etStk/pub?output=csv&single=true&gid=";
 
-var req = http.get(url, function(response) {
-   var csv = "";
-   response.setEncoding('utf8');
-   response.on('data', function(text) {
-      csv += text;
+function getContent(gid) {
+   return new Promise(function(resolve, reject) {
+      var request = http.get(urlBase + gid, function(response) {
+         var csv = "";
+         response.setEncoding('utf8');
+         response.on('data', function(text) {
+            csv += text;
+         });
+
+         response.on('end', function() {
+            resolve(csv);
+         });
+      });
+
+      request.on("error", function(error) {
+         reject(error);
+      });
    });
+} //getContent
 
-   response.on('end', function() {
-      console.log(parse(csv));
-   });
-});
-
-req.on("error", function(error) {
-   console.log(error);
+getContent(0).then(function(x) {
+   console.log(x);
 });
